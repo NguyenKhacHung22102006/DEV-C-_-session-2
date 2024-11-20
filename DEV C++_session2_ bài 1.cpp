abda0c2fd28ccb4be2a8156681741bbe1fb4a749
NguyenKhacHung22102006 <hungskyok06@gmail.com>
2024-11-20 23:57:46 +0700

    Update DEV C++_session2_ bài 1.cpp

--- DEV C++_session2_ bài 1.cpp
+++ DEV C++_session2_ bài 1.cpp
@@ -7,7 +7,5 @@ int main()
     float DiemTB; //kieu so thuc
     char TenSV; //kieu ki tu
     double MaSV;//kieu so thuc nhung lon hon
-    printf("pham_nhat_tan_s1_ex1\n");
-    printf("Hello world!\n");
     return 0;
 }
